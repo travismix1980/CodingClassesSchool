d7a699504f45d966e39e21c4126c746b6c768865
Travis Mix <travismix1980@gmail.com>
2023-04-22 16:58:25 -0700

    Finished Prep4 list activity for week 2

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
-using System;
-
-class Program
+namespace lists
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello Prep4 World!");
+        static void Main(string[] args)
+        {
+            int num = Int32.MaxValue;
+            List<int> numList = new List<int>();
+            while (num != 0) {
+                Console.Write("Please Enter a list of numbers or Enter 0 to finish: ");
+                num = Int32.Parse(Console.ReadLine());
+                if (num != 0)
+                {
+                    numList.Add(num);
+                }
+            }
+
+            // output sum of numList
+            Console.WriteLine($"The sum of all the numbers in our list is {numList.Sum()}");
+
+            // output average of numList
+            Console.WriteLine($"The average of the numbers in the list is {numList.Average()}");
+
+            // output the max of numList
+            Console.WriteLine($"The max number in the list is {numList.Max()}");
+
+            // get smallest positive number in numList will output 0 if there is no positive numbers in list
+            int minPositive = numList.Where(x => x > 0 ).DefaultIfEmpty(0).Min();
+            Console.WriteLine($"The smallest positive number in the list is {minPositive}.");
+
+            // sort and display the new sorted numList
+            numList.Sort();
+            Console.WriteLine("Here is the list but sorted from smallest to largest");
+            foreach(int n in numList.SkipLast(1))
+            {
+                Console.Write($"{n}, ");
+            }
+            // output last number in list
+            Console.WriteLine($"{numList.Last()}");
+        }
     }
 }
