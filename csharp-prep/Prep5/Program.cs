662d814285c0e9c8edc885699c8562a72df35dad
Travis Mix <travismix1980@gmail.com>
2023-04-22 17:10:42 -0700

    Finished Prep5 functions activity for week 2

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), PromptUserNumber());
+    }
+
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName(){
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("Please enter your favorite number: ");
+        return Int32.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number){
+        return (int)Math.Pow(number, 2);
+    }
+
+    static void DisplayResult(string name, int number){
+        int squared = SquareNumber(number);
+        Console.WriteLine($"{name}, the square of your favorite number is {squared}.");
     }
 }
