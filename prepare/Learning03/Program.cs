c5f79be7cb1617af386ea9f2fd9a847dba7aabbd
Travis Mix <travismix1980@gmail.com>
2023-05-14 17:00:05 -0700

    Completed Week 5 Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,29 @@
-using System;
-
-class Program
+namespace Week5Encapsulation
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Main(string[] args)
+        {
+            Fraction f1 = new();
+            Fraction f2 = new(5);
+            Fraction f3 = new(3, 4);
+            Fraction f4 = new(1, 3);
+
+            Console.WriteLine("Fraction 1");
+            Console.WriteLine(f1.GetFractionString());
+            Console.WriteLine(f1.GetDecimalValue());
+
+            Console.WriteLine("Fraction 2");
+            Console.WriteLine(f2.GetFractionString());
+            Console.WriteLine(f2.GetDecimalValue());
+
+            Console.WriteLine("Fraction 3");
+            Console.WriteLine(f3.GetFractionString());
+            Console.WriteLine(f3.GetDecimalValue());
+
+            Console.WriteLine("Fraction 4");
+            Console.WriteLine(f4.GetFractionString());
+            Console.WriteLine(f4.GetDecimalValue());
+        }
     }
 }
