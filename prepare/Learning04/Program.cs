949bd8bbed6507b7abd136028d27ad0eaf5e9267
Travis Mix <travismix1980@gmail.com>
2023-05-30 16:46:32 -0700

    Finished Week 7 Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,18 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new("Samuel Bennet", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();    // spacing
+
+        MathAssignment ma = new("Roberto Rodrigues", "Fractions", "7.3", "8-19");
+        Console.WriteLine(ma.GetSummary());
+        Console.WriteLine(ma.GetHomeworkList());
+        Console.WriteLine();    // spacing
+
+        WritingAssignment wa = new("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(wa.GetSummary());
+        Console.WriteLine(wa.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,11 @@
+class WritingAssignment : Assignment{
+  private string _title;
+
+  public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic) {
+    _title = title;
+  }
+
+  public string GetWritingInformation(){
+    return $"{_title} by {_studentName}";
+  }
+}
