756d0c3249c9f2dc98299ee778044b90ff6142a8
Travis Mix <travismix1980@gmail.com>
2023-07-10 01:58:41 -0700

    Card Class Complete

--- final/FinalProject/Card.cs
+++ final/FinalProject/Card.cs
@@ -6,7 +6,26 @@ public class Card{
   public Card(string suit, string pips){
     _suit = suit;
     _pips = pips;
-
+    _pipsValue = new Dictionary<string, int>();
+    _pipsValue.Add("One", 1);
+    _pipsValue.Add("Two", 2);
+    _pipsValue.Add("Three", 3);
+    _pipsValue.Add("Four", 4);
+    _pipsValue.Add("Five", 5);
+    _pipsValue.Add("Six", 6);
+    _pipsValue.Add("Seven", 7);
+    _pipsValue.Add("Eight", 8);
+    _pipsValue.Add("Nine", 9);
+    _pipsValue.Add("Ten", 10);
+    _pipsValue.Add("Jack", 10);
+    _pipsValue.Add("Queen", 10);
+    _pipsValue.Add("King", 10);
   }
-  public int HandleAces(){ return 0; }
+  public int HandleAces(int handTotal){
+    if(handTotal > 10){
+      return 1;
+    } else {
+      return 11;
+    }
+  }
 }
