2fd6a7cb80ab5881e77acfc8c023776fe90bcd3d
Travis Mix <travismix1980@gmail.com>
2023-07-13 07:19:11 -0700

    Fixed deck creation to make it simpler to work with and the dealer can now deal

--- final/FinalProject/Dealer.cs
+++ final/FinalProject/Dealer.cs
@@ -1,30 +1,51 @@
 public class Dealer : Person{
   private string[] _names = {"Bobby", "Sam", "Susan","Sally"};  // array to randomly chose a name for the dealer from
-  private List<Deck> _decks; // list of all the decks the dealer has access to
+  private List<Card> _decks; // list of all the decks the dealer has access to
+  private int _numOfDecks;
 
   public Dealer(int numOfDecks){
+    _numOfDecks = numOfDecks;
     _decks = new();
     // get random name
     Random rand = new();
     int nameIndex = rand.Next(_names.Length);
     base.SetName(_names[nameIndex]);
     // add decks to the dealers group of decks
-    for(int i = 0; i < numOfDecks; i++){
-      _decks.Add(new Deck());
-    }
+    SetupDeck();
   }
 
-// TODO: Make the dealer able to deal a single card
-  public void Deal(){
-    Console.WriteLine("Dealer deals everyone a card");
+  public void SetupDeck(){
+    _decks.Clear(); // clear _decks so we dont have extra cards
+    for(int i = 0; i < _numOfDecks; i++){
+      // get a deck and add each card from that deck
+      Deck d = new();
+      foreach(var c in d.GetDeck()){
+        _decks.Add(c);
+      }
+    }
+    // a real dealer would shuffle multiple times a vegas dealer shuffles three times
+    Shuffle();
+    Shuffle();
+    Shuffle();
+  }
+
+
+  public Card Deal(){
+    // if not enough cards re setup decks vegas dealers tend to shuffle between
+    // 40 and 60 percent of cards remaining I went with 40%
+    if(_decks.Count <= (_numOfDecks * 52 * .40)){
+      SetupDeck();
+    }
+    // deal card
+    Card c = _decks[0];
+    _decks.RemoveAt(0);
+    return c;
   }
 
   public void Shuffle(){
-    foreach(var deck in _decks){
+    foreach(var card in _decks){
       Random rand = new();
-      var d = deck.GetDeck();
-      var sd = d.OrderBy( _ => rand.Next()).ToList();
-      deck.SetDeck(sd);
+      _decks = _decks.OrderBy( _ => rand.Next()).ToList();
     }
   }
 
